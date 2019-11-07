2ef6fd06be7590714ce24f17adf8c54f890b1a2d
Weitang Li <liwt31@163.com>
2019-11-07 21:29:57 +0800

    update exp1

--- lib.py
+++ lib.py
@@ -1,15 +1,17 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def cart_contour(theta, rho, a):
-    plt.contourf(theta, rho, a, 100, cmap="RdBu")
+def cart_contour(rho, theta, a):
+    m = np.max(np.abs(a))
+    plt.contourf(theta, rho, a, 100, cmap="RdBu", vmin=-m, vmax=m)
     plt.colorbar()
     plt.show()
 
-def polar_contour(theta, r, values):
+def polar_contour(r, theta, values):
+    m = np.max(np.abs(values))
     fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
     ax.grid(False)
     ax.set_theta_offset(np.pi/2)
-    ax.contourf(theta, r, values, 100, cmap="RdBu")
+    ax.contourf(theta, r, values, 100, cmap="RdBu", vmin=-m, vmax=m)
     plt.axis('off')
     plt.show()
